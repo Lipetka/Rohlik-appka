2c83d5f31e679939a114b6464a96008b3abd3a1f
Lipetka <liptak2000@gmail.com>
2023-01-29 14:08:35 +0100

    Add text print for verification

--- Ver.2/functions/readPDFContents.py
+++ Ver.2/functions/readPDFContents.py
@@ -7,11 +7,11 @@ File that returns content of PDF file (can be used for any PDF i guess)
 
 import PyPDF2
 
-def getContents(fileName = "Ver.2\pdfFileName.pdf"):
+def getContents(fileName = "Ver.2/files/pdfFileName.pdf"):
 
     # importing PDF file
 
-    pdf_file_object   = open( fileName,'rb')    # open file name from GUI (dropdown or drag n drop)
+    pdf_file_object   = open(fileName,'rb')    # open file name from GUI (dropdown or drag n drop)
     pdf_read          = PyPDF2.PdfFileReader(pdf_file_object)   # extract PDF contents
 
     page_count       = pdf_read.numPages  # count pages to loop through them
@@ -24,6 +24,7 @@ def getContents(fileName = "Ver.2\pdfFileName.pdf"):
         page = pdf_read.getPage(i) # get nth page
         output_text = output_text + page.extractText() # read text and append it
 
+    print(output_text + "\n\n")
     return output_text  # return output text
 
 
--- Ver.2/receiptItemsExtractor.py
+++ Ver.2/receiptItemsExtractor.py
@@ -4,15 +4,18 @@ This file containts code to extract items from receipt
 and save contents in dictionary
 
 """
+import functions.readPDFContents as readPDFContents  # file that contains function to extract PDF text
 
-# imports
 
-import readPDFContents  # file that contains function to extract PDF text
+receipt_text = (
+    readPDFContents.getContents()
+)  # enter filename or leave empty for default test file
 
-receipt_text    = readPDFContents.getContents()   # enter filename or leave empty for default test file
-bough_items_dictionary = receipt_items   = readPDFContents.extractOrderedItems(receipt_text) # enter receipt text and save output as dictionary
+bough_items_dictionary = readPDFContents.extractOrderedItems(
+    receipt_text
+)  # enter receipt text and save output as dictionary
+
 
 # check total sum to verify skript
-total_sum = round(sum(bough_items_dictionary.values()),2)
-print(total_sum)
-
+total_sum = round(sum(bough_items_dictionary.values()), 2)
+print("Total sum: " + str(total_sum))
