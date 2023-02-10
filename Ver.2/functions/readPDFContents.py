049e1008f9520134b87c7e99e519913cc9aac466
Lipetka <liptak2000@gmail.com>
2023-02-10 12:14:09 +0100

    deleted version 1

--- Ver.2/functions/readPDFContents.py
+++ Ver.2/functions/readPDFContents.py
@@ -1,28 +1,21 @@
-"""
-File that returns content of PDF file (can be used for any PDF i guess)
-
-"""
-
-# imports
-
 import PyPDF2
 
-def getContents(fileName = "Ver.2/files/pdfFileName.pdf"):
+def getContents(file_name = "Ver.2/files/pdfFileName.pdf"):
 
     # importing PDF file
 
-    pdf_file_object   = open(fileName,'rb')    # open file name from GUI (dropdown or drag n drop)
-    pdf_read          = PyPDF2.PdfFileReader(pdf_file_object)   # extract PDF contents
+    pdf_file_object     = open(file_name,'rb')                       # open file name from GUI (dropdown or drag n drop)
+    pdf_read            = PyPDF2.PdfFileReader(pdf_file_object)     # extract PDF contents
 
-    page_count       = pdf_read.numPages  # count pages to loop through them
+    page_count          = pdf_read.numPages                         # count pages to loop through them
 
-    output_text     = "" # placeholder for text output        
+    output_text         = ""                                        # placeholder for text output        
 
     for i in range(page_count):
         # loop through all pages and save the output to output_text variable
 
-        page = pdf_read.getPage(i) # get nth page
-        output_text = output_text + page.extractText() # read text and append it
+        page            = pdf_read.getPage(i)                   # get nth page
+        output_text     = output_text + page.extractText()      # read text and append it
 
     print(output_text + "\n\n")
     return output_text  # return output text
@@ -41,7 +34,7 @@ def extractOrderedItems(receipt_text):
     items_dictionary    = {}            # final dictionary placeholder
 
     for i in range(length_of_list):
-        # loop throught all items 
+        # loop throught all items
 
         # protection against empty lines and file end
         if items[i] == '':
@@ -50,21 +43,21 @@ def extractOrderedItems(receipt_text):
             break
 
         # check if the first letter is digit
-        if items[i][0].isdigit() == False:
+        if not items[i][0].isdigit():
 
             # if first letter isnt digit, check if the second line starts with digit
             # to catch multiline names
 
-            if items[i+1][0].isdigit() == False:
+            if not items[i+1][0].isdigit():
                 name = items[i] + items[i+1]    # if the w line is also letter, save both lines as a name
             else:
                 name = items[i] # if following line is number, save only the first line as name
 
 
         # solving for number
-        if items[i][0].isdigit() == True:
+        if items[i][0].isdigit():
             # if the next line starts with letter save the price
-            if items[i+1][0].isdigit() == False:
+            if not items[i+1][0].isdigit():
                 price = float(items[i].replace(',','.').split()[0]) # save the number
                 items_dictionary.update({name:price})   # add new item to dictionary when price is found
             else:
